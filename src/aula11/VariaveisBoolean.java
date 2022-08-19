431798bd2c455c1415cd39c0426a3da3f9e8f37c
Pedro Arruda <pedroarruda8@live.com>
2022-08-19 16:59:37 -0300

    Loaine Java

---
+++ src/aula11/VariaveisBoolean.java
@@ -0,0 +1,14 @@
+package aula11;
+
+public class VariaveisBoolean {
+
+    public static void main(String[] args) {
+
+        boolean verdadeiro = true;
+
+        boolean falso = false;
+
+        System.out.println("O valor verdadeiro é " + verdadeiro);
+        System.out.println("O valor de false é " + falso);
+    }
+}
---
+++ src/aula24/labs/Livro.java
@@ -0,0 +1,10 @@
+package aula24.labs;
+
+public class Livro {
+
+    String nome;
+    String autor;
+    int qtdPaginas;
+    int anoLancamento;
+    String isbn;
+}
