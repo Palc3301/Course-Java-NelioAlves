de977ba999cdecab05d70735046fac2712180520
Pedro Arruda <pedroarruda8@live.com>
2022-09-07 14:57:47 -0300

    End of basic java course.

--- src/aula52/UsandoMinhaException.java
+++ src/aula52/UsandoMinhaException.java
@@ -13,15 +13,15 @@ public class UsandoMinhaException {
 
     public static void teste() throws DivisaoNaoExata{
         int[] numeros = {4, 8, 5, 16, 32, 21, 64, 128};
-        int[] demon = {2, 0, 4, 8, 0, 2, 4};
+        int[] denom = {2, 0, 4, 8, 0, 2, 4};
 
         for (int i=0; i<numeros.length; i++){
             try{
                 if (numeros[i] % 2 != 0){
                     //lançar a exception aqui
-                    throw new DivisaoNaoExata(numeros[i], demon[i]);
+                    throw new DivisaoNaoExata(numeros[i], denom[i]);
                 }
-                System.out.println(numeros[i] + "/" + demon[i] + " = " + (numeros[i]/demon[i]));
+                System.out.println(numeros[i] + "/" + denom[i] + " = " + (numeros[i]/denom[i]));
             }
             catch(ArithmeticException | ArrayIndexOutOfBoundsException e){
                 System.out.println("Aconteceu um erro");
